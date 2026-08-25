66c0ea18116aabee9685f9c1fddd35aec9f53c4d
BrambillaR <roberto.brambilla02@st.com>
2026-08-26 00:41:20 +0200

    codeMR first test

---
+++ iot/codemr/iot.controller/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"iot", "Number of Highly Problematic Classes":0, "Number of Entities":8, "Number of Problematic Classes":0, "Class Lines of Code":147, "Number of External Packages":6, "Number of Packages":1, "Number of External Entities":23}
---
+++ iot/codemr/iot.controller/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,5 @@
+var matrix = [[0]]
+var packages = [{
+"name": " com.example.iot.controller", "color": " #3182bd"
+}
+];
